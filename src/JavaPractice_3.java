eadd2b8f18fcaa7773439fc863192eb6a1eec6a3
mkerror98 <mkerror98@gmail.com>
2023-01-23 14:22:51 +0900

    커밋 확인용 java파일 삭제, ch2 생

--- src/JavaPractice_3.java
+++
@@ -1,6 +0,0 @@
-public class JavaPractice_3 {
-	public static void main(String[] args) {
-		System.out.println("I like coding.");
-	}
-
-}
---
+++ src/ch2_variable/VarEx1.java
@@ -0,0 +1,22 @@
+// 두 개의 변수 age와 year를 선언한 다음, 값을 저장하고 출력해보자.
+package ch2_variable;
+
+public class VarEx1 {
+
+	public static void main(String[] args) {
+		int age = 26;
+		int year = 0;
+		
+		System.out.println(age);
+		System.out.println(year);
+		
+		year = 2000 + 26;
+		age = age + 1;
+		
+		System.out.println(year);
+		System.out.println(age);
+		
+		
+	}
+
+}
