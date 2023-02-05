423c042be0ed61a86f155a246dce7bf602d3ff4d
mkerror98 <mkerror98@gmail.com>
2023-02-05 22:16:35 +0900

    인스턴스를 생성하고 인스턴스의 속성과 메서드를 사용해보자.

---
+++ src/ch6_oop1/TvTest.java
@@ -0,0 +1,24 @@
+package ch6_oop1;
+
+class Tv {
+	// Tv의 속성(멤버변수) 	
+	String color;			// 색상.
+	boolean power;			// 전원상태.
+	int channel;			// 채널.
+	
+	//Tv의 기능(메서드)
+	void power()	 	{power = !power;}		// Tv의 전원을 끄거냐 켜는 메서드.
+	void channelUp() 	{++channel;}
+	void channelDown()	{--channel;}	
+}
+
+public class TvTest {
+	public static void main(String[] args) {
+		Tv t;
+		t = new Tv();
+		t.channel = 7;
+		t.channelDown();
+		System.out.println("현재 채널은 " + t.channel + "입니다.");
+	}
+
+}
