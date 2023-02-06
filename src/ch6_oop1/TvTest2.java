8e1dfd4db8ee2711abdf25b6103f4068defd0c82
mkerror98 <mkerror98@gmail.com>
2023-02-06 15:41:31 +0900

    Tv6클래스의 인스턴스 t1과 t2를 생성한 후에, t1의 channel값을 변경하고 t1과 t2를 비교해보자.

--- src/ch6_oop1/TvTest2.java
+++ src/ch6_oop1/TvTest2.java
@@ -1,6 +1,6 @@
 package ch6_oop1;
 
-class Tv6 {
+class Tv2 {
     // Tv의 속성(멤버변수)
     String color;           // 색상
     boolean power;          // 전원상태(on.off)
@@ -14,8 +14,8 @@ class Tv6 {
 
 public class TvTest2 {
     public static void main(String[] args) {
-        Tv6 t1 = new Tv6();       // Tv t1; t1 = new Tv();를 한 문장으로 가능.
-        Tv6 t2 = new Tv6();
+        Tv2 t1 = new Tv2();       // Tv t1; t1 = new Tv();를 한 문장으로 가능.
+        Tv2 t2 = new Tv2();
         System.out.println("t1의 channel값은 " + t1.channel + "입니다.");
         System.out.println("t2의 channel값은 " + t2.channel + "입니다.");
 
