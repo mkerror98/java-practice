5c8849678e13ffc892aaeecfbb62f615a2b93e8b
mkerror98 <mkerror98@gmail.com>
2023-02-06 23:50:30 +0900

    객체 배열 예제

---
+++ src/ch6_oop1/TvTest4.java
@@ -0,0 +1,30 @@
+package ch6_oop1;
+
+public class TvTest4 {
+    public static void main(String[] args) {
+        Tv4[] tvArr = new Tv4[3];   // 길이가 3인 Tv객체 배열
+
+        // Tv객체를 생성해서 Tv객체 배열의 각 요소에 저장
+        for(int i=0; i< tvArr.length; i++) {
+            tvArr[i] = new Tv4();
+            tvArr[i].channel = i+10;   // tvArr[i]의 channel에 i+10을 저장
+        }
+
+        for(int i=0; i< tvArr.length; i++) {
+            tvArr[i].channelUp();    // tvArr[i]의 메서드를 호출. 채널이 1 증가
+            System.out.printf("tvArr[%d].channel=%d\n",i,tvArr[i].channel);
+        }
+    }
+}
+
+class Tv4 {
+    String color;
+    boolean power;
+    int channel;
+
+    void power()  {power=!power;}
+    void channelUp()  { ++channel;}
+    void channelDown()  {--channel;}
+
+
+}
