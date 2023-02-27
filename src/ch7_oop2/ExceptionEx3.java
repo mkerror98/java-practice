bfd28f084cf565fc76a23a691e6bb71a3c77b5ba
mkerror98 <mkerror98@gmail.com>
2023-02-27 10:58:10 +0900

    try - catch문을 사용한 예외처리

---
+++ src/ch7_oop2/ExceptionEx3.java
@@ -0,0 +1,17 @@
+package ch7_oop2;
+
+public class ExceptionEx3 {
+    public static void main(String[] args) {
+        int number = 100;
+        int result = 0;
+
+        for(int i=0; i<10; i++) {
+            try {
+                result = number/(int)(Math.random() * 10);
+                System.out.println(result);
+            } catch (ArithmeticException ae) {
+                System.out.println("0");    // 나누는 숫자가 0이라면 ArithmeticException이 발생하므로, 0이 출력되도록 예외처리 하였음.
+            } // try-catch의 끝
+        } // for문의 끝.
+    }
+}
